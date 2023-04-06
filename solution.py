7a8d0b5480cb8a49a60eab601b4f4b49a9c0dce0
navipudge666 <33724392+navipudge666@users.noreply.github.com>
2023-04-06 18:09:08 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,4 +9,4 @@ chat_id = 475225606  # Ваш chat ID, не меняйте название пе
 def solution(p: float, x: np.array) -> tuple:
     n = x.shape[0]
     m = x.mean()
-    return m/8 - 0.25 + gamma.ppf((1 - p)/2, n) / (2 * n), m/8 - 0.25 + gamma.ppf((1 + p)/2, n) / (2 * n)
+    return m/8 - 0.0625 + gamma.ppf((1 - p)/2, n) / (2 * n), m/8 - 0.0625 + gamma.ppf((1 + p)/2, n) / (2 * n)
